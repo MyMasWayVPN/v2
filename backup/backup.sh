3b4013abbb3d8ebbecc113645420376f073060cd
Khoirul Bahra <107555389+MyMasWayVPN@users.noreply.github.com>
2023-03-09 13:34:13 +0700

    Update backup.sh

--- backup/backup.sh
+++ backup/backup.sh
@@ -111,7 +111,7 @@ zip -rP $InputPass $NameUser.zip backup > /dev/null 2>&1
 ##############++++++++++++++++++++++++#############
 LLatest=`date`
 Get_Data () {
-git clone https://github.com/myridwan/backup.git /root/user-backup/ &> /dev/null
+git clone https://github.com/MyMasWayVPN/Backup.git /root/user-backup/ &> /dev/null
 }
 
 Mkdir_Data () {
@@ -131,15 +131,15 @@ mv /root/$NameUser.zip /root/user-backup/$NameUser/
 Save_And_Exit () {
     DATE=$(date +'%d %B %Y')
     cd /root/user-backup
-    git config --global user.email "alnurridwan112@gmail.com" &> /dev/null
-    git config --global user.name "myridwan" &> /dev/null
+    git config --global user.email "khoirul110604@gmail.com" &> /dev/null
+    git config --global user.name "MyMasWayVPN" &> /dev/null
     rm -rf .git &> /dev/null
     git init &> /dev/null
     git add . &> /dev/null
     git commit -m backup &> /dev/null
     git branch -M main &> /dev/null
-    git remote add origin https://github.com/myridwan/backup
-    git push -f https://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@github.com/myridwan/backup.git &> /dev/null
+    git remote add origin https://github.com/MyMasWayVPN/Backup
+    git push -f https://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@github.com/MyMasWayVPN/Backup.git &> /dev/null
 }
 
 if [ ! -d "/root/user-backup/" ]; then
@@ -154,7 +154,7 @@ sleep 1
 echo -e "$COLOR1│${NC}  [INFO] Processing updating server...... "
 Save_And_Exit
 fi
-link="https://raw.githubusercontent.com/myridwan/backup/ipuk/$NameUser/$NameUser.zip"
+link="https://raw.githubusercontent.com/MyMasWayVPN/Backup/main/$NameUser/$NameUser.zip"
 sleep 1
 echo -e "$COLOR1│${NC}  [INFO] Backup done "
 sleep 1
